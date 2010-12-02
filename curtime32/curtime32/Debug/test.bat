eae07e71be62664a3dc324a21ff851ef6fd93824
sergii.kulyk1 <sergii.kulyk1@bae701b9-234b-92aa-4aaa-9a2066c1bf2b>
2010-12-02 15:22:12 +0000

    add new files

---
+++ curtime32/curtime32/Debug/test.bat
@@ -0,0 +1,1 @@
+curtime32.exe "$y$M$d $T $-1s -1s$c$y$M$d $T $-1m -1m$c$y$M$d $T $-1h -1h$c$y$M$d $T $-1d -1d$c$y$M$d $T $+1s +1s$c$y$M$d $T $+1m +1m$c$y$M$d $T $+1h +1h$c$y$M$d $T $+1d +1d$c$y$M$d $T$c"
